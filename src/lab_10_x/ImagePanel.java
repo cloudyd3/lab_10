1beca6c0c1a88fa1f37e81b77fd13212ae745333
Aliks Popov <hedhero@gmail.com>
2020-01-22 06:11:31 +0300

    minor changes

--- src/lab_10_x/ImagePanel.java
+++ src/lab_10_x/ImagePanel.java
@@ -24,10 +24,10 @@ public class ImagePanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         AffineTransform at = new AffineTransform();
-        at.translate(getWidth() / 2, getHeight() / 2);
+        at.translate(getWidth() >> 1, getHeight() >> 1);
         at.rotate(Math.toRadians(rotAngle));
-        at.scale(0.75,0.75);
-        at.translate(-image.getWidth()/2, -image.getHeight()/2);
+        at.scale(0.7,0.7);
+        at.translate(-image.getWidth() >> 1, -image.getHeight() >> 1);
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawImage(image, at, null);
     }
